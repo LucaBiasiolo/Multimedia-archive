e95d9694d1c474151f8f889be52e362183b112d1
Luca Biasiolo <lcbiasiolo@gmail.com>
2020-06-14 16:44:23 +0200

    14-06-20: Uso database invece di dizionario per file nuovi

--- new_files.py
+++ new_files.py
@@ -1,15 +1,16 @@
-#Il seguente script prende i file nuovi da aggiungere da una cartella, elimina quelli con estensioni non adatte e li rinomina opportunamente andando a pescare il numero progressivo giusto dall'archivio
-
+#Il seguente script prende i file nuovi da aggiungere da una cartella, elimina quelli con estensioni non adatte e li rinomina e sposta opportunamente andando a pescare il numero progressivo giusto dal database associato all'archivio
+import sqlite3
 import os
 from datetime import datetime
 path=r"C:\Users\utente\Pictures\2020-06-02"#=input("Inserisci path con foto e video: ")
 archivepath=r"C:\Users\utente\Desktop\Luca\Archivio"
 eofs=[".jpg",".JPG",".jpeg",".png",".gif",".mp4",".opus",".mpeg"]
-archivedict={}
+conn=sqlite3.connect("archive.db")
+c=conn.cursor()
 
 newfolder=os.scandir(path)
 newfiles=[]
-for file in newfiles:
+for file in newfolder:
     truefalse=list()
     for eof in eofs:
         truefalse.append(file.name.endswith(eof))
@@ -18,38 +19,33 @@ for file in newfiles:
     else:
         print("Impossibile processare",file.name,"in quanto estensione non valida")
 
-#trovo numero progressivo
-years=os.scandir(archivepath)
-for year in years:
-    if len(year.name)==4:
-        archivedict[year.name]={}
-        months=os.scandir(year.path)
-        for month in months:
-            if month.is_dir():
-                archivedict[year.name][month.name]={} #dizionario con giorni e num max raggiunto
-                files=os.scandir(month.path)
-                for file in files:
-                    if len(file.name.split("-"))==4:
-                        day=file.name.split("-")[0]
-                        archivedict[year.name][month.name][day]=archivedict[year.name][month.name].get(day,0)+1 #popola il dizionario
-
-#rinomino i file con data di ultima modifica e li sposto   
-newfiles=os.scandir(path)             
-for file in newfiles:
-    if file.name!="Thumbs.db":
-        mtimestamp=os.path.getmtime(file.path)
-        mdate=datetime.fromtimestamp(mtimestamp) #data ultima modifica file
-        day=str(mdate.day)
-        month=str(mdate.month)
-        year=str(mdate.year)
-        eof=file.name.split(".")[1] #estensione file
-        try:
-            number=archivedict[year][month][day]=archivedict[year][month].get(day,0)+1 #popola il dizionario
-        except:
-            print("Provo a creare la cartella anno\mese \\%s\\%s" %(year,month))
-            os.mkdir(archivepath+"\\%s\\%s" %(year,month)) #creo cartella anno/mese se non esiste
-            archivedict[year][month]={}
-            number=archivedict[year][month][day]=archivedict[year][month].get(day,0)+1 #popola il dizionario
-        newname="%s-%s-%s-%s.%s" %(day,month,year[2:],str(number),eof)
-        #print("Rinomino ",file.name,"come",newname,"e lo sposto in ",archivepath+"\\%s\\%s\\%s" %(year,month,newname))
-        #os.rename(file.path,archivepath+"\\%s\\%s\\%s" %(year,month,newname)) #sposto i nuovi file
+#rinomino i file con data di ultima modifica e li sposto              
+for file in newfiles: #rinominare prima usando il nome del file e dopo con la data di ultima modifica in caso non si riuscisse
+    mtimestamp=os.path.getmtime(file.path)
+    mdate=datetime.fromtimestamp(mtimestamp) #data ultima modifica file
+    day=mdate.day
+    month=mdate.month
+    year=str(mdate.year)
+    eof=file.name.split(".")[1] #estensione file
+    c.execute("select max(Prog_number) from Files where Day=? and Month=? and Year=?",(day,month,year[2:]))
+    maxpn=c.fetchone()[0]
+    if maxpn is None:
+        number=1
+    else:
+        number=maxpn+1
+    if file.name.endswith(".opus"):
+        type_flag=1
+    elif file.name.endswith(".mp4") or file.name.endswith(".mpeg"):
+        type_flag=2
+    else:
+        type_flag=0
+    newname="%s-%s-%s-%s.%s" %(day,month,year[2:],str(number),eof)
+    print("Rinomino ",file.name,"come",newname,"e lo sposto in ",archivepath+"\\%s\\%s\\%s" %(year,month,newname))
+    #os.rename(file.path,archivepath+"\\%s\\%s\\%s" %(year,month,newname)) #sposto i nuovi file
+    #creare cartella anno/mese se lo spostamento non va a buon fine
+    
+    #aggiorno database per aggiornare automaticamente numero progressivo
+    #print("insert into Files values (%s,%d,%d,%s,%d,%s,%s,%d)" %(newname,day,month,year[2:],number,archivepath+"\\%s\\%s\\%s" %(year,month,newname),eof,type_flag))
+    c.execute("insert into Files values (?,?,?,?,?,?,?,?,?)",(None,newname,day,month,year[2:],number,archivepath+"\\%s\\%s\\%s" %(year,month,newname),eof,type_flag))
+conn.rollback()
+conn.close()
