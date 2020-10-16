cb50d861b4d4b9313e3c882396663736bf99cfee
Luca Biasiolo <lcbiasiolo@gmail.com>
2020-10-16 15:17:57 +0200

    Change access modifiers and create getters and setters for ArchiveFile

--- Java/src/biasiolo/luca/archive/ArchiveFile.java
+++ Java/src/biasiolo/luca/archive/ArchiveFile.java
@@ -2,17 +2,50 @@ package biasiolo.luca.archive;
 
 public class ArchiveFile extends NewFile {
 
-    protected int day;
-    protected int month;
-    protected int year;
-    protected int progressiveNumber;
+    private int day;
+    private int month;
+    private int year;
+    private int progressiveNumber;
+    public static final String[] ADMITTED_FILE_EXTENSIONS = {"jpg", "JPG", "jpeg", "png", "gif", "mp4", "opus", "mpeg", "mp3"};
 
     public ArchiveFile(String path) {
         super(path);
-        this.day = Integer.parseInt(this.getName().split("-")[0]);
-        this.month = Integer.parseInt(this.getName().split("-")[1]);
-        this.year = Integer.parseInt(this.getName().split("-")[2]);
+        this.day = Integer.parseInt(this.getName().split("-")[0]));
+        this.month = Integer.parseInt(this.getName().split("-")[1]));
+        this.year = Integer.parseInt(this.getName().split("-")[2]));
         String finalPiece = this.getName().split("-")[3];
         this.progressiveNumber = Integer.parseInt(finalPiece.split("\\.")[0]);
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getProgressiveNumber() {
+        return progressiveNumber;
+    }
+
+    public void setProgressiveNumber(int progressiveNumber) {
+        this.progressiveNumber = progressiveNumber;
     }
 }
