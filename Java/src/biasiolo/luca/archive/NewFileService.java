70062976fef800ef1dc8458f6d6b23a3ceb45d44
Luca Biasiolo <lcbiasiolo@gmail.com>
2020-10-19 12:32:27 +0200

    Methods in NewFileService are non-static and improved code

--- Java/src/biasiolo/luca/archive/NewFileService.java
+++ Java/src/biasiolo/luca/archive/NewFileService.java
@@ -3,13 +3,14 @@ package biasiolo.luca.archive;
 import java.io.File;
 import java.sql.*;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class NewFileService {
 
     private static NewFileService newFileServiceInstance = null;
-
-    private NewFileService() {
-    }
+    private final Logger logger = Logger.getLogger("New-file-service-logger");
+    private NewFileService() {}
 
     public static NewFileService getInstance() {
         if (newFileServiceInstance == null) {
@@ -18,28 +19,16 @@ public class NewFileService {
         return newFileServiceInstance;
     }
 
-    public static boolean checkFile(NewFile newFile) throws SQLException {
+    public boolean checkFileExtension(NewFile newFile) throws SQLException {
         boolean okay = true;
-        if (!Arrays.asList(Archive.ADMITTED_FILE_EXTENSIONS).contains(newFile.fileExtension)) { // Se l'estensione del file non è tra quelle ammesse
-            System.out.println("Impossibile processare " + newFile.getName() + " in quanto estensione non valida");
+        if (!Arrays.asList(ArchiveFile.ADMITTED_FILE_EXTENSIONS).contains(newFile.fileExtension)) {
+            logger.log(Level.INFO,"Impossibile processare " + newFile.getName() + " in quanto estensione non valida");
             okay = false;
-        } else {
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:archive.db");
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(String.format(
-                    "select file_name from files where hash=%d", newFile.hashCode
-            ));
-            if (resultSet.next()) {
-                String fileName = resultSet.getString("File_name");
-                System.out.println(newFile.getName() + " doppione di " + fileName);
-                okay = false;
-            }
-            connection.close();
         }
         return okay;
     }
 
-    public static void renameAndMoveFile(NewFile newFile) throws SQLException {
+    public void renameAndMoveFile(NewFile newFile) throws SQLException {
         if (newFile.getName().split("\\.")[0].length() == 10) {
             renameTimestampFile(newFile);
         } else if (newFile.getName().startsWith("IMG-") || newFile.getName().startsWith("VID-")) {
@@ -51,6 +40,7 @@ public class NewFileService {
         } else {
             renameMDateFile(newFile);
         }
+        // databaseService.getProgNumber(day,month,year);
         Connection connection = DriverManager.getConnection("jdbc:sqlite:archive.db");
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(String.format(
@@ -75,35 +65,35 @@ public class NewFileService {
         connection.close();
     }
 
-    private static void renameYearMonthDayHourFile(NewFile newFile) {
+    private void renameYearMonthDayHourFile(NewFile newFile) {
         String[] pieces = newFile.getName().split("_");
         newFile.year = Integer.parseInt(pieces[0].substring(0, 4));
         newFile.month = Integer.parseInt(pieces[0].substring(4, 6).replaceFirst("^0+(?!$)", ""));
         newFile.day = Integer.parseInt(pieces[0].substring(6).replaceFirst("^0+(?!$)", ""));
     }
 
-    private static void renameTimestampFile(NewFile newFile) {
+    private void renameTimestampFile(NewFile newFile) {
         Timestamp timestamp = new Timestamp(Integer.parseInt(newFile.getName().split("\\.")[0]));
         newFile.day = timestamp.getDay();
         newFile.month = timestamp.getMonth();
         newFile.year = timestamp.getYear();
     }
 
-    private static void renameIMGVIDFile(NewFile newFile) {
+    private void renameIMGVIDFile(NewFile newFile) {
         String[] pieces = newFile.getName().split("-");
         newFile.year = Integer.parseInt(pieces[1].substring(0, 4));
         newFile.month = Integer.parseInt(pieces[1].substring(4, 6).replaceFirst("^0+(?!$)", ""));
         newFile.day = Integer.parseInt(pieces[1].substring(6).replaceFirst("^0+(?!$)", ""));
     }
 
-    private static void renameWPFile(NewFile newFile) {
+    private void renameWPFile(NewFile newFile) {
         String[] pieces = newFile.getName().split("_");
         newFile.year = Integer.parseInt(pieces[1].substring(0, 4));
         newFile.month = Integer.parseInt(pieces[1].substring(4, 6).replaceFirst("^0+(?!$)", ""));
         newFile.day = Integer.parseInt(pieces[1].substring(6).replaceFirst("^0+(?!$)", ""));
     }
 
-    private static void renameMDateFile(NewFile newFile) {
+    private void renameMDateFile(NewFile newFile) {
         Timestamp lastModifiedTimestamp = new Timestamp(new File(newFile.getAbsolutePath()).lastModified());
         newFile.day = lastModifiedTimestamp.getDay();
         newFile.month = lastModifiedTimestamp.getMonth();
