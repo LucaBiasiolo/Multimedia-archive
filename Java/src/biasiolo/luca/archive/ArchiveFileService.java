811e662711d576d9753342cec8f44463b1118025
Luca Biasiolo <lcbiasiolo@gmail.com>
2020-10-19 12:38:00 +0200

    Removed unused code and Database is now a singleton

--- Java/src/biasiolo/luca/archive/ArchiveFileService.java
+++ Java/src/biasiolo/luca/archive/ArchiveFileService.java
@@ -1,8 +1,5 @@
 package biasiolo.luca.archive;
 
-import java.sql.*;
-import java.util.ArrayList;
-
 public class ArchiveFileService {
 
     private static ArchiveFileService archiveFileServiceInstance = null;
@@ -16,36 +13,7 @@ public class ArchiveFileService {
         return archiveFileServiceInstance;
     }
 
-    private void renameArchiveFiles() throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:sqlite:archive.db");
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select path from files");
-        ArrayList<ArchiveFile> archiveFiles = null;
-        if (resultSet.next()) {
-            String filePath = resultSet.getString("path");
-            ArchiveFile archiveFile = new ArchiveFile(filePath);
-            boolean okay = NewFileService.checkFile(archiveFile);
-            if (okay) {
-                // TODO
-            }
-        }
+    private void renameArchiveFiles(){
         // TODO: utilizzare direttamente i path assoluti dei file invece di navigare nelle cartelle
     }
-
-//        public void rename() throws SQLException {
-//        Statement statement = connection.createStatement();
-//        ResultSet resultSet = statement.executeQuery(String.format(
-//                "select max (prog_number) from files where day =%s and month=%s and year=%s",this.day,this.month,this.year
-//        ));
-//        if (resultSet.next()){
-//            String maxProgressiveNumber = resultSet.getString("prog_number");
-//            if (maxProgressiveNumber.equals("NULL")) {
-//                this.progressiveNumber = Integer.parseInt(maxProgressiveNumber) + 1;
-//            } else {
-//                this.progressiveNumber = Integer.parseInt("1");
-//            }
-//        }
-//        String archiveFileName = this.day + "-" + this.month + "-" + Integer.toString(this.year).substring(2) + "-" + this.progressiveNumber + "-" + this.fileExtension;
-//        // TODO: implementare interazione con il sistema operativo
-    //}
 }
--- Java/src/biasiolo/luca/archive/Database.java
+++ Java/src/biasiolo/luca/archive/Database.java
@@ -1,18 +1,15 @@
 package biasiolo.luca.archive;
 
-import java.sql.*;
-
 public class Database {
 
-    public Database() throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:sqlite:archive.db");
-        Statement statement = connection.createStatement();
-        statement.executeQuery("drop table Files");
-        statement.executeQuery(
-                "create table if not exists files (file_id integer primary key, file_name text unique, day integer, month integer, year integer, prog_number integer, eof text, path text unique, hash text unique)"
-        );
-        connection.close();
+    private static Database databaseInstance = null;
+
+    private Database(){}
+
+    public static Database getInstance() {
+        if (databaseInstance == null) {
+            databaseInstance = new Database();
+        }
+        return databaseInstance;
     }
-
-
 }
