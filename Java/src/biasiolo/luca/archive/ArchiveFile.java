6b49b0c2851a96731905ebf7abd412a089639b88
Luca Biasiolo <lcbiasiolo@gmail.com>
2020-10-19 12:36:01 +0200

    Fixed parenthesis

--- Java/src/biasiolo/luca/archive/ArchiveFile.java
+++ Java/src/biasiolo/luca/archive/ArchiveFile.java
@@ -10,9 +10,9 @@ public class ArchiveFile extends NewFile {
 
     public ArchiveFile(String path) {
         super(path);
-        this.day = Integer.parseInt(this.getName().split("-")[0]));
-        this.month = Integer.parseInt(this.getName().split("-")[1]));
-        this.year = Integer.parseInt(this.getName().split("-")[2]));
+        this.day = Integer.parseInt(this.getName().split("-")[0]);
+        this.month = Integer.parseInt(this.getName().split("-")[1]);
+        this.year = Integer.parseInt(this.getName().split("-")[2]);
         String finalPiece = this.getName().split("-")[3];
         this.progressiveNumber = Integer.parseInt(finalPiece.split("\\.")[0]);
     }
