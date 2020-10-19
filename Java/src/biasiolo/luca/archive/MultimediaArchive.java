79c9ee90a27bd8630821f667b8158b78cbb1b5dd
Luca Biasiolo <lcbiasiolo@gmail.com>
2020-10-19 12:33:02 +0200

    Delete unused code to be implemented in the future

--- Java/src/biasiolo/luca/archive/MultimediaArchive.java
+++ Java/src/biasiolo/luca/archive/MultimediaArchive.java
@@ -8,19 +8,6 @@ import java.util.Scanner;
 public class MultimediaArchive {
 
     public static void main(String[] args) throws SQLException {
-        System.out.println("Inserisci path assoluto della cartella da aggiungere: \n");
-        Scanner scanner = new Scanner(System.in);
-        String newFilesPath = scanner.nextLine();
-        File directory = new File(newFilesPath);
-        for (File file : Objects.requireNonNull(directory.listFiles())) {
-            NewFile newFile = new NewFile(file.getAbsolutePath());
-            boolean okay = NewFileService.checkFile(newFile);
-            if (okay) {
-                NewFileService.renameAndMoveFile(newFile);
-            }
-        }
+        System.out.println("Hello world!");
     }
-    // TODO: cancellare cartella java da branch master e risolvere merge-conflicts con il commit funzionante
-    // TODO: implementare metodo che chieda all'utente dove creare l'archivio, con che nome
-    // TODO: implementare GUI che permetta all'utente di eseguire le varie funzioni
 }
