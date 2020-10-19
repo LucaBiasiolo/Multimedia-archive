10eff2dd2d64d6537785cbb31324aa5e0b052016
Luca Biasiolo <lcbiasiolo@gmail.com>
2020-10-19 12:37:25 +0200

    Archive is now a singleton and add root-path checking

--- Java/src/biasiolo/luca/archive/Archive.java
+++ Java/src/biasiolo/luca/archive/Archive.java
@@ -1,16 +1,29 @@
 package biasiolo.luca.archive;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Archive {
 
+    private static Archive archiveInstance = null;
     public static String ARCHIVE_ROOT_PATH;
+    private final Logger logger = Logger.getLogger("Archive-logger");
+
+    private Archive(){}
+
+    public static Archive getInstance() {
+        if (archiveInstance == null) {
+            archiveInstance = new Archive();
+        }
+        return archiveInstance;
+    }
 
     public void setArchiveRootPath(String path) {
         if (ARCHIVE_ROOT_PATH == null) {
             this.ARCHIVE_ROOT_PATH = path;
         }
         else{
-            // TODO: da trasformare in log di errore
-            System.out.println("Errore: root path dell'archivio già impostato");
+            logger.log(Level.SEVERE, "Error: archive root path has already been set");
         }
     }
 }
