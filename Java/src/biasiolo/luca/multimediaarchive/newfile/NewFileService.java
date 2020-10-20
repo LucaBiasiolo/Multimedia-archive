2bfa44336014c6a3eed24837a514e3204482a720
Luca Biasiolo <lcbiasiolo@gmail.com>
2020-10-20 10:59:21 +0200

    Added processNewFile method and improved code structure

--- Java/src/biasiolo/luca/multimediaarchive/newfile/NewFileService.java
+++ Java/src/biasiolo/luca/multimediaarchive/newfile/NewFileService.java
@@ -1,7 +1,6 @@
 package biasiolo.luca.multimediaarchive.newfile;
 
 import biasiolo.luca.multimediaarchive.MultimediaArchive;
-import biasiolo.luca.multimediaarchive.archive.ArchiveFile;
 import biasiolo.luca.multimediaarchive.database.DatabaseService;
 
 import java.io.File;
@@ -13,8 +12,9 @@ import java.util.logging.Logger;
 public class NewFileService {
 
     private static NewFileService newFileServiceInstance = null;
-    private final Logger logger = Logger.getLogger("New-file-service-logger");
-    private final Properties properties = MultimediaArchive.properties;
+    private Properties properties = MultimediaArchive.properties;
+    private String[] ADMITTED_FILE_EXTENSIONS = properties.getProperty("ADMITTED_FILE_EXTENSIONS").split(",");
+    private final Logger logger = Logger.getLogger("biasiolo.luca.multimediaarchive.newfile.NewFileService");
     private final DatabaseService databaseService = DatabaseService.getInstance();
     private int day;
     private int month;
@@ -29,9 +29,20 @@ public class NewFileService {
         return newFileServiceInstance;
     }
 
-    public boolean checkFileExtension(NewFile newFile) throws SQLException {
+    public void processNewFile(NewFile newFile){
+        checkFileExtension(newFile);
+        renameFile(newFile);
+        // TODO: implementare creazione cartelle dell'anno e del mese se queste ancora non esistono
+        // TODO: rinominare, effettuare i controlli per lo spostamento, spostare e infine aggiungere dati al db
+        // TODO: implementare rinominazione file e spostamento nella cartella opportuna dell'archivio
+        // checkArchive()
+        // moveFile(newFile);
+        // addFileToDb()
+    }
+
+    public boolean checkFileExtension(NewFile newFile) {
         boolean okay = true;
-        if (!Arrays.asList(ArchiveFile.ADMITTED_FILE_EXTENSIONS).contains(newFile.fileExtension)) {
+        if (!Arrays.asList(ADMITTED_FILE_EXTENSIONS).contains(newFile.fileExtension)) {
             logger.info( newFile.getName() + " cannot be processed as its extension is invalid");
             okay = false;
         }
@@ -39,7 +50,6 @@ public class NewFileService {
     }
 
     public void renameFile(NewFile newFile) {
-        int[] datePieces = new int[3];
         if (newFile.getName().split("\\.")[0].length() == 10) {
             renameTimestampFile(newFile);
         } else if (newFile.getName().startsWith("IMG-") || newFile.getName().startsWith("VID-")) {
@@ -54,17 +64,10 @@ public class NewFileService {
         this.progressiveNumber = databaseService.getProgNumber(this.day,this.month,this.year);
         String newFileName = this.day + "-" + this.month + "-" + Integer.toString(this.year).substring(2) + "-"
                 + this.progressiveNumber + "." + newFile.fileExtension;
-        newFile.renameTo(new File(properties.getProperty("NEW_FOLDER_PATH")+ newFileName)); // rinomino
-        // checkArchive()
-        // move()
-        // addFileToDb()
-        // TODO: rinominare, effettuare i controlli per lo spostamento, spostare e infine aggiungere dati al db
-        // TODO: implementare rinominazione file e spostamento nella cartella opportuna dell'archivio
+        newFile.renameTo(new File(properties.getProperty("NEW_FOLDER_PATH")+ newFileName));
     }
 
     private void moveFile(NewFile newFile){
-        // TODO: implementare creazione cartelle dell'anno e del mese se queste ancora non esistono
-        newFile.renameTo(new File("path di arrivo"));
     }
 
     private void renameYearMonthDayHourFile(NewFile newFile) {
