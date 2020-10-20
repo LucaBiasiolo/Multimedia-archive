b9f0c65ddca2d7454840d8b9aca47c1b71869ece
Luca Biasiolo <lcbiasiolo@gmail.com>
2020-10-20 10:23:05 +0200

    Use properties and ARCHIVE_ROOT_PATH is set only once

--- Java/src/biasiolo/luca/multimediaarchive/archive/Archive.java
+++ Java/src/biasiolo/luca/multimediaarchive/archive/Archive.java
@@ -1,13 +1,16 @@
 package biasiolo.luca.multimediaarchive.archive;
 
-import java.util.logging.Level;
+import biasiolo.luca.multimediaarchive.MultimediaArchive;
+
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public class Archive {
 
     private static Archive archiveInstance = null;
-    public static String ARCHIVE_ROOT_PATH;
     private final Logger logger = Logger.getLogger("Archive-logger");
+    private Properties properties = MultimediaArchive.properties;
+    public String ARCHIVE_ROOT_PATH = properties.getProperty("ARCHIVE_ROOT_PATH");
 
     private Archive(){}
 
@@ -16,14 +19,5 @@ public class Archive {
             archiveInstance = new Archive();
         }
         return archiveInstance;
-    }
-
-    public void setArchiveRootPath(String path) {
-        if (ARCHIVE_ROOT_PATH == null) {
-            this.ARCHIVE_ROOT_PATH = path;
-        }
-        else{
-            logger.log(Level.SEVERE, "Error: archive root path has already been set");
-        }
     }
 }
