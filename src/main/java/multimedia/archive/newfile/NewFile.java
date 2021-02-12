29208c69d088847a800d7fbcec386ed950d3d8eb
Luca Biasiolo <luca.biasiolo@akera.it>
2021-02-12 18:01:45 +0100

    Cambiata posizione costruttore e proprietà fileExtension

--- src/main/java/multimedia/archive/newfile/NewFile.java
+++ src/main/java/multimedia/archive/newfile/NewFile.java
@@ -4,18 +4,18 @@ import java.io.File;
 
 public class NewFile extends File {
 
+    protected String fileExtension;
+
+    public NewFile(String path) {
+        super(path);
+        this.fileExtension = this.getName().split("\\.")[1];
+    }
+
     public String getFileExtension() {
         return fileExtension;
     }
 
     public void setFileExtension(String fileExtension) {
         this.fileExtension = fileExtension;
-    }
-
-    protected String fileExtension;
-
-    public NewFile(String path) {
-        super(path);
-        this.fileExtension = this.getName().split("\\.")[1];
     }
 }
