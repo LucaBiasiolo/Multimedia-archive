60866571cb8fa84355b8bf0231ca0dea20128ced
Luca Biasiolo <luca.biasiolo@akera.it>
2021-02-15 15:15:14 +0100

    Eliminati file inutilizzati e riscritta definizione db usando PostgreSQL

--- src/main/resources/archive-ddl.sql
+++
@@ -1,3 +0,0 @@
-create table if not exists files 
-	(file_id integer primary key, file_name text unique, day integer, month integer, 
-	year integer, prog_number integer, eof text, path text unique, hash text unique)
