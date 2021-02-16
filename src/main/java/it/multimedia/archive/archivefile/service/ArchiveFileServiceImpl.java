15d2f67f676e1c05457c23e7c7e206f146514653
Luca Biasiolo <luca.biasiolo@akera.it>
2021-02-16 18:20:42 +0100

    Creato service per ArchiveFile con semplice logica di passacarte

---
+++ src/main/java/it/multimedia/archive/archivefile/service/ArchiveFileServiceImpl.java
@@ -0,0 +1,20 @@
+package it.multimedia.archive.archivefile.service;
+
+import it.multimedia.archive.archivefile.repository.ArchiveFileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ArchiveFileServiceImpl implements ArchiveFileService {
+
+    @Autowired
+    private ArchiveFileRepository archiveFileRepository;
+
+    @Transactional(readOnly = true)
+    public List getArchiveFiles() {
+        return archiveFileRepository.getArchiveFiles();
+    }
+}
