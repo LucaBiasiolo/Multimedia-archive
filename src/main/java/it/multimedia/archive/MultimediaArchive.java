6e72934c51ebc2f38d85ce68cdeda5bb425f16f1
Luca Biasiolo <luca.biasiolo@akera.it>
2021-02-17 14:33:50 +0100

    Cancellato codice inutilizzato

--- src/main/java/it/multimedia/archive/MultimediaArchive.java
+++ src/main/java/it/multimedia/archive/MultimediaArchive.java
@@ -1,6 +1,5 @@
 package it.multimedia.archive;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.Scanner;
@@ -32,9 +31,5 @@ public class MultimediaArchive {
         System.out.println("Absolute path of the folder in which to create the archive");
         final String ARCHIVE_ROOT_PATH = scanner.nextLine();
         scanner.close();
-        Properties properties = new Properties();
-        properties.setProperty("NEW_FOLDER_PATH", NEW_FOLDER_PATH);
-        properties.setProperty("ARCHIVE_ROOT_PATH", ARCHIVE_ROOT_PATH);
-        properties.store(new FileWriter("multimediaArchive.properties"),null);
     }
 }
