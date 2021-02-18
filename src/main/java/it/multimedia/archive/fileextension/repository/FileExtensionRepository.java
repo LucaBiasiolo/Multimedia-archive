bae923dca758fb526d35390a86abbf6c613d2ebd
Luca Biasiolo <luca.biasiolo@akera.it>
2021-02-18 18:24:50 +0100

    Aggiunti semplici metodi per operazioni CRUD su FileExtension

--- src/main/java/it/multimedia/archive/fileextension/repository/FileExtensionRepository.java
+++ src/main/java/it/multimedia/archive/fileextension/repository/FileExtensionRepository.java
@@ -1,8 +1,16 @@
 package it.multimedia.archive.fileextension.repository;
 
+import it.multimedia.archive.fileextension.FileExtension;
+
 import java.util.List;
 
 public interface FileExtensionRepository {
 
     List getFileExtensions();
+
+    FileExtension insertFileExtension(FileExtension fileExtension);
+
+    FileExtension modifyFileExtension(FileExtension updatedFileExtension);
+
+    FileExtension deleteFileExtensionByName(String extensionName);
 }
--- src/main/java/it/multimedia/archive/fileextension/repository/FileExtensionRepositoryImpl.java
+++ src/main/java/it/multimedia/archive/fileextension/repository/FileExtensionRepositoryImpl.java
@@ -1,7 +1,9 @@
 package it.multimedia.archive.fileextension.repository;
 
+import it.multimedia.archive.fileextension.FileExtension;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -19,5 +21,32 @@ public class FileExtensionRepositoryImpl implements FileExtensionRepository {
 
     public List getFileExtensions() {
         return session().createQuery("from FileExtension").list();
+    }
+
+    // TODO: 18/02/2021 Creare metodi CRUD per estensioni file
+
+    public FileExtension insertFileExtension(FileExtension fileExtension) {
+        session().persist(fileExtension);
+        return fileExtension;
+    }
+
+    public FileExtension modifyFileExtension(FileExtension updatedFileExtension) {
+        FileExtension fileExtensionToUpdate = session().get(FileExtension.class, updatedFileExtension.getId());
+        if (fileExtensionToUpdate != null) {
+            fileExtensionToUpdate.setExtension(updatedFileExtension.getExtension());
+            return fileExtensionToUpdate;
+        } else {
+            return null;
+        }
+    }
+
+    public FileExtension deleteFileExtensionByName(String extensionName) {
+        Query query = session().createQuery("from FileExtension where extension = :name");
+        query.setParameter("name", extensionName);
+        FileExtension extension = (FileExtension) query.uniqueResult();
+        if (extension != null) {
+            session().delete(extension);
+        }
+        return extension;
     }
 }
