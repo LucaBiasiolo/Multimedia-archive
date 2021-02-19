5ba4979c5209e3581ba98db6c36070213cdcb63d
Luca Biasiolo <luca.biasiolo@akera.it>
2021-02-19 15:45:39 +0100

    Creata eccezione per numero progressivo non trovato

---
+++ src/main/java/it/multimedia/archive/exceptions/ProgressiveNumberNotFoundException.java
@@ -0,0 +1,8 @@
+package it.multimedia.archive.exceptions;
+
+public class ProgressiveNumberNotFoundException extends Throwable {
+    public ProgressiveNumberNotFoundException(int day, int month, int year) {
+        super(String.format("ProgressiveNumberNotFoundException: Un numero progressivo per la data %d-%d-%s " +
+                "non è stato trovato", day, month, year));
+    }
+}
