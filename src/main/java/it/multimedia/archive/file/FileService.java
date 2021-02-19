3639eef021d02ac94bd93781e392c825af16be49
Luca Biasiolo <luca.biasiolo@akera.it>
2021-02-19 11:18:29 +0100

    Aggiunti metodi all'interfaccia

--- src/main/java/it/multimedia/archive/file/FileService.java
+++ src/main/java/it/multimedia/archive/file/FileService.java
@@ -1,4 +1,10 @@
 package it.multimedia.archive.file;
 
+import java.io.File;
+
 public interface FileService {
+
+    boolean checkFileExtension(File newFile);
+
+    void renameFile(File newFile);
 }
