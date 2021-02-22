0c15e17aa0224937b8c1967e6c24c80a1cc19f11
Luca Biasiolo <luca.biasiolo@akera.it>
2021-02-22 10:16:16 +0100

    Aggiunti metodi get e insert per oggetti Archive

--- src/main/java/it/multimedia/archive/repository/ArchiveRepository.java
+++ src/main/java/it/multimedia/archive/repository/ArchiveRepository.java
@@ -1,4 +1,12 @@
 package it.multimedia.archive.repository;
 
+import it.multimedia.archive.Archive;
+
+import java.util.List;
+
 public interface ArchiveRepository {
+
+    List getArchives();
+
+    Archive insertArchive(Archive archive);
 }
--- src/main/java/it/multimedia/archive/repository/ArchiveRepositoryImpl.java
+++ src/main/java/it/multimedia/archive/repository/ArchiveRepositoryImpl.java
@@ -1,7 +1,29 @@
 package it.multimedia.archive.repository;
 
+import it.multimedia.archive.Archive;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class ArchiveRepositoryImpl implements ArchiveRepository {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    private Session session() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    public List getArchives() {
+        return session().createQuery("from Archive").list();
+    }
+
+    public Archive insertArchive(Archive archive) {
+        session().persist(archive);
+        return archive;
+    }
 }
