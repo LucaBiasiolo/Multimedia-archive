b8899ac3e22b9087d7b58c46a9364d57f469f4fd
Luca Biasiolo <luca.biasiolo@akera.it>
2021-02-23 17:55:18 +0100

    Aggiunto archivio test a db

--- src/main/resources/multimedia-archive-dump.sql
+++ src/main/resources/multimedia-archive-dump.sql
@@ -47,3 +47,6 @@ create table anag_archive_files (
 
 insert into anag_file_extensions(file_extension)
 	values ('jpg'), ('JPG'), ('jpeg'), ('png'), ('gif'), ('mp4'), ('opus'), ('mpeg'), ('mp3');
+	
+insert into anag_archives(archive_name, archive_path)
+	values ('Prova', 'Path prova');
