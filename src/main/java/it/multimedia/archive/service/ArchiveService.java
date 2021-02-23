713566bfadc814ffbc8ea09b5196a83347dbc069
Luca Biasiolo <luca.biasiolo@akera.it>
2021-02-23 17:54:00 +0100

    Aggiunto semplice metodo passacarte per recuperare lista archivi

--- src/main/java/it/multimedia/archive/service/ArchiveService.java
+++ src/main/java/it/multimedia/archive/service/ArchiveService.java
@@ -1,4 +1,10 @@
 package it.multimedia.archive.service;
 
+import it.multimedia.archive.Archive;
+
+import java.util.List;
+
 public interface ArchiveService {
+
+    List<Archive> getArchives();
 }
--- src/main/java/it/multimedia/archive/service/ArchiveServiceImpl.java
+++ src/main/java/it/multimedia/archive/service/ArchiveServiceImpl.java
@@ -1,7 +1,21 @@
 package it.multimedia.archive.service;
 
+import it.multimedia.archive.Archive;
+import it.multimedia.archive.repository.ArchiveRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ArchiveServiceImpl implements ArchiveService {
+
+    @Autowired
+    private ArchiveRepository archiveRepository;
+
+    @Transactional(readOnly = true)
+    public List<Archive> getArchives() {
+        return archiveRepository.getArchives();
+    }
 }
