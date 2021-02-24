8552b26ebb0bccb29078d4eb24a5b3d3e43ffa63
Luca Biasiolo <luca.biasiolo@akera.it>
2021-02-24 15:44:42 +0100

    Implementato metodo per inviare archivio al fe usando l'id

--- src/main/java/it/multimedia/archive/web/controllers/ArchiveController.java
+++ src/main/java/it/multimedia/archive/web/controllers/ArchiveController.java
@@ -5,6 +5,7 @@ import it.multimedia.archive.service.ArchiveService;
 import it.multimedia.archive.web.MultimediaArchiveAPIResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,6 +21,15 @@ public class ArchiveController {
         MultimediaArchiveAPIResponse<List<Archive>> response = new MultimediaArchiveAPIResponse<>();
         response.setStatus("OK");
         response.setResponse(archiveService.getArchives());
+        return response;
+    }
+
+    @GetMapping(value = "/archives/{id}")
+    private MultimediaArchiveAPIResponse<Archive> getArchiveById(@PathVariable(value = "id") long archiveId) {
+        MultimediaArchiveAPIResponse<Archive> response = new MultimediaArchiveAPIResponse<>();
+        Archive requestedArchive = archiveService.getArchiveById(archiveId);
+        response.setStatus("OK");
+        response.setResponse(requestedArchive);
         return response;
     }
 }
